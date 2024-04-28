83abd022d8f0e67793724fe678194610655106b9
quan <quan_xiaoxiao@qq.com>
2024-04-28 16:23:27 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,13 @@
 import generateRouteList from './generateRouteList.mjs';
 import filterRoutesOfPathname from './filterRoutesOfPathname.mjs';
 import getRouteOfRequest from './getRouteOfRequest.mjs';
+import generateRouteMatchList from './generateRouteMatchList.mjs';
+import createHttpRequestHooks from './createHttpRequestHooks.mjs';
 
 export {
   generateRouteList,
   getRouteOfRequest,
   filterRoutesOfPathname,
+  generateRouteMatchList,
+  createHttpRequestHooks,
 };
